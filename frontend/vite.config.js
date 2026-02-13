4778fcff4848e1bf2f509ecb6d97bd804898f133
Business <businessmoana118@gmail.com>
2026-02-13 12:49:36 +0200

    frontend port change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8888,
+    port: 8080,
     host: true, // allow ngrok/network access
     allowedHosts: ['.ngrok-free.dev', '.ngrok.io', '.ngrok-free.app'],
     proxy: {
